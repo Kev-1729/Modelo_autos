4c4cd07b7e9bedfcfd8854716a629d98d36c4902
Kevin Tupac Agüero <96688861+Kev-1729@users.noreply.github.com>
2025-05-18 23:09:45 -0500

    Update req.py

--- req.py
+++ req.py
@@ -1,7 +1,5 @@
 import requests
-
 url = "http://127.0.0.1:8000/predecir/"
-
 data = {
     "Marca": "Toyota",
     "Combustible": "Gasolina",
@@ -9,6 +7,5 @@ data = {
     "Kilometraje": 50000,
     "Antiguedad": 3
 }
-
 response = requests.post(url, json=data)
 print(response.json())
